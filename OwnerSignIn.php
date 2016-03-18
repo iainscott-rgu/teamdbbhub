580e8c68b467c198cf96e8d7c888206abf0f8661
Iain Scott <odyssey3007@hotmail.com>
2016-03-18 21:53:00 +0000

    here we go again!

--- OwnerSignIn.php
+++ OwnerSignIn.php
@@ -207,8 +207,8 @@ HTMLPAGE;
 
     <tr><td>You are on the owner's review page!</td></tr>
 
-    <tr><td></td><td><a href="B&Bregistration.php"><input type = 'submit' value = 'Add B&B'/></a></td></tr>
-    <tr><td></td><td><a href="bbReviewPage.php"><input type = 'submit' value = 'Review Your B&Bs'/></a></td></tr>
+    <tr><td></td><td><a href="B&Bregistration.php"><input class="btn2" type = 'submit' value = 'Add B&B'/></a></td></tr>
+    <tr><td></td><td><a href="bbReviewPage.php"><input class="btn2" type = 'submit' value = 'Review Your B&Bs'/></a></td></tr>
 
 
 
