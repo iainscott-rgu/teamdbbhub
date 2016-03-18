680e94bef6fd2cf0a5c0a45d785314854a69d30c
Iain Scott <odyssey3007@hotmail.com>
2016-03-18 18:53:07 +0000

    here we go again!

--- Customerinfo.php
+++ Customerinfo.php
@@ -238,14 +238,14 @@ catch(PDOException $e)
                 </td>
                 <td>
                     <label for="email">Email: *</label></td>
-                <td><input type="text" id="email" class="inputform" name="email" placeholder="email" size="30" maxlength="50" required /></td>
+                <td><input type="text" id="email" class="inputform" name="email" placeholder="Enter your Email Address" size="30" maxlength="50" required /></td>
             </tr>
             <tr>
                 <td><label for="firstname">First Name: *</label></td>
                 <td><input type="text" id="firstname" class="inputform" name="firstname" value="Enter your First Name" required /></td>
                 <td>
                     <label for="telephone">Telephone: *</label></td>
-                <td><input type="text" id="telephone" class="inputform" name="telephone" placeholder=" Enter your telephone number" size="20" maxlength="20" required /></td>
+                <td><input type="text" id="telephone" class="inputform" name="telephone" placeholder="Enter your telephone number" size="20" maxlength="20" required /></td>
             </tr>
             </tr>
             <tr>
