898fb5176753d0bb611191b5bb54bcde2cd1dcab
Iain Scott <odyssey3007@hotmail.com>
2016-03-18 03:44:02 +0000

    here we go again!

--- searchResultsPage.php
+++ searchResultsPage.php
@@ -235,7 +235,7 @@ session_start();
                 $newhtml =
                     <<<NEWHTML
                         <div class="table5">
-<a href="Customerinfo.php"><table border="0" cellpadding="5">
+<a href="Customerinfo.php"><table id="table5" border="0" cellpadding="5">
 <tr>
 <td><strong><img class="bbimages" src="{$row[imageurl]}" id="img3"></strong></td>
 <td>
